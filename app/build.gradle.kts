28c6c4727723e970c01f85d75931e5c302f91b79
vikash853github <svikashkumar853@gmail.com>
2024-01-13 11:35:35 +0530

    initial commit2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.androidgithub"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.androidgithub"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
